acfc075e4fb592fdee4aec61b8d497e02febac74
Peter Grillot <petergrillot@gmail.com>
2017-06-20 14:09:57 -0400

    Works on my system, for now

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-#! python3
+#!/bin/env/python
 
 # Import Modules
-import tkinter
 from tkinter import *
 import subprocess
 import shlex
@@ -13,16 +12,16 @@ root = Tk()
 
 # Instructions
 root.title('MouseTrap')
-Label(text='Set Mouse Speed \nUses GTK Setting, 1 = Lowest <-> 10 = Highest',justify=LEFT).pack(side=TOP,padx=settings.padding,pady=settings.padding)
+Label(text='Set Mouse Speed \nUses GTK Setting, 0 = Faster <-> 10 = Slower',justify=LEFT).pack(side=TOP,padx=settings.padding,pady=settings.padding)
 
-scale = Scale(root, from_=0, to=100,orient=HORIZONTAL)
+scale = Scale(root, from_=0, to=5,orient=HORIZONTAL,resolution=0.1)
 scale.pack(side=TOP,padx=settings.padding,pady=settings.padding)
 
 # Get Mouse Settings from GUI
 def setMouse():
 		n = str(scale.get())
-		subprocess.call(["echo",n]);
-
+		#subprocess.call(["echo",n]);
+		subprocess.call(['xinput','--set-prop','15','Device Accel Constant Deceleration',n])
 Button(root, text='Ok', command=setMouse).pack(side=LEFT,padx=settings.padding,pady=settings.padding)
 Button(root, text="Close",command=root.destroy).pack(side=RIGHT,padx=settings.padding,pady=settings.padding)
 
